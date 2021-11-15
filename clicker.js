da162490f0506fad9ad1db3f3521bc05be31e31b
AugustinKrb <93907781+AugustinKrb@users.noreply.github.com>
2021-11-15 09:40:54 +0100

    Mise à jour des variables nbrClick et nbrChips

--- clicker.js
+++ clicker.js
@@ -1,7 +1,10 @@
 var nbrClick = 0;
 var nbrChips = 0;
 var bonus = ["marteau", "coup de poing"];
+var tabBonusMultiplicateur = ["10", "4"];
 var bonusActifs = initialiserBonus(bonus);
+
+var bonusMultiplicateur = 1;
 
 console.log(bonusActifs);
 
@@ -16,4 +19,10 @@ function initialiserBonus(tab) {
         res.push(false);
     }
     return res;
+}
+
+function chipsCassee() {
+    nbrClick ++;
+    nbrChips += bonusMultiplicateur;
+    console.log("nbr click : " + nbrClick + " et nbrChips : " + nbrChips);
 }
