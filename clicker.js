8720687644491fc679e3a8beb8d273f6822a19e9
AugustinKrb <93907781+AugustinKrb@users.noreply.github.com>
2021-11-15 11:02:43 +0100

    Affichage des boutons

--- clicker.js
+++ clicker.js
@@ -1,17 +1,12 @@
 var nbrClick = 0;
 var nbrChips = 0;
-var bonus = ["marteau", "coup de poing"];
-var tabBonusMultiplicateur = ["10", "4"];
-var bonusActifs = initialiserBonus(bonus);
+var tabIdBonus = ["Coupdepoing", "marteau", "pistolet"]
+var tabBonus = ["\uD83D\uDC4A", "\uD83D\uDD28", "\uD83D\uDD2B"];
+var tabCout = ["20", "40", "60"];
+var tabBonusMultiplicateur = ["4", "10", "20"];
+var tabBonusActifs = initialiserBonus(tabBonus);
 
 var bonusMultiplicateur = 1;
-
-console.log(bonusActifs);
-
-
-
-
-
 
 function initialiserBonus(tab) {
     let res = [];
@@ -25,4 +20,27 @@ function chipsCassee() {
     nbrClick ++;
     nbrChips += bonusMultiplicateur;
     console.log("nbr click : " + nbrClick + " et nbrChips : " + nbrChips);
+    actualisationAffichageClickEtChips();
+}
+
+function actualisationAffichageClickEtChips() {
+    var tmp = document.getElementById("nbrClick")
+    document.getElementById("nbrClick").innerHTML=nbrClick;
+    document.getElementById("nbrChips").innerHTML=nbrChips;
+}
+
+function affichageBonusDisponibles() {
+    for (let i = 0; i < tabBonusMultiplicateur.length; i++) {        
+        var button = document.createElement("button");
+        var nameButton = document.createTextNode(tabBonus[i] + " (" + tabCout[i] + ")");
+        button.id = tabIdBonus[i];
+        button.type = "button";
+        button.addEventListener("click", test);
+        button.appendChild(nameButton);
+        document.getElementById("bonusDispo").appendChild(button);
+    }
+}
+
+function test() {
+    console.log("coucou, je suis le onclicker");
 }
