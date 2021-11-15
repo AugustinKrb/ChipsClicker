9cbfe6ca0c759fc80bdc7c6872e07afc76bbc06d
AugustinKrb <93907781+AugustinKrb@users.noreply.github.com>
2021-11-15 09:25:42 +0100

    Initialisation des 4 variables

--- clicker.js
+++ clicker.js
@@ -1,3 +1,19 @@
-<script>
-    
-</script>
+var nbrClick = 0;
+var nbrChips = 0;
+var bonus = ["marteau", "coup de poing"];
+var bonusActifs = initialiserBonus(bonus);
+
+console.log(bonusActifs);
+
+
+
+
+
+
+function initialiserBonus(tab) {
+    let res = [];
+    for (let i = 0; i < tab.length; i++) {
+        res.push(false);
+    }
+    return res;
+}
